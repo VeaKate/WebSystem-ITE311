b77f32afbfba24ec1c94172517de7853d6b58649
al287 <johnlloydalcayde870@gmail.com>
2025-08-27 20:00:05 +0800

    Added Home controller, routes, and views for basic navigation

--- ITE311-ALCAYDE/app/Config/Routes.php
+++ ITE311-ALCAYDE/app/Config/Routes.php
@@ -5,6 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->get('/', 'Home::index');
 $routes->get('/index', 'Home::index');
 $routes->get('/about', 'Home::about');
 $routes->get('/contact', 'Home::contact');
